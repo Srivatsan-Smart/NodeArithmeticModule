a3ddc1cb493b8d5dc3cf616920c55f7a2ddfde91
unknown <anirudhvatsan@gmail.com>
2020-03-24 15:50:57 +0530

    Add Environment Vars

---
+++ Desktop/Secrets/app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+  extended : true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true,useUnifiedTopology : true});
+
+
+
+
+const userSchema = new mongoose.Schema({
+  email : String,
+  password : String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields : ["password"]});
+
+const User = mongoose.model("User",userSchema);
+
+userSchema.plugin
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+
+app.post("/register",function(req,res){
+  const user = new User({
+    email : req.body.username,
+    password : req.body.password
+  });
+
+  user.save(function(err){
+    if(err) console.log(err);
+    else {
+      res.render("secrets");
+    }
+  });
+});
+
+
+app.post("/login",function(req,res){
+  const email = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email : email},function(err,foundUser){
+    if(err) console.log(err);
+    else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+        else{
+          res.send("Login Failed");
+        }
+      }
+      else{
+        res.send("Login Failed");
+      }
+    }
+  });
+});
+app.listen("3000",function(){
+  console.log("Server started at 3000");
+});
